6788f220b4287c5e95f0c5b30055cd3253e4d777
Felix Springer <felixspringer149@gmail.com>
2019-08-17 02:38:28 +0200

    ordered functions

--- src/GameState.hs
+++ src/GameState.hs
@@ -28,6 +28,10 @@ data GameState b p = GStarted b p
                    | GState b p b Int
                    | GEnded b p
 
+class Game b c p => State b c p where
+
+  display :: b -> String
+
 startGame :: forall b c p. Game b c p => IO (b,p)
 startGame = runGame board player
   where board = empty :: b
@@ -41,15 +45,11 @@ runGame board player = do -- putStr $ display board player
                               newPlayer = next player
                           runGame newBoard newPlayer
 
-endGame :: forall b c p. State b c p => GameState b p -> IO ()
-endGame (GEnded board player) = putStr "end"
-
 stepGame :: forall b c p. State b c p => GameState b p -> IO (GameState b p)
 stepGame (GStarted board player) = return (GStarted board player)
 
-class Game b c p => State b c p where
-
-  display :: b -> String
+endGame :: forall b c p. State b c p => GameState b p -> IO (b,p)
+endGame (GEnded board player) = putStr "end" >> return (board , player)
 
 readIOSafe :: (String -> Maybe (Action c)) -> IO (Action c)
 readIOSafe reader = reader <$> getLine >>= maybe (readIOSafe reader) return
