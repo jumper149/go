ef382a5fb2df005951ed4f8bdf4c1628306b1c91
Felix Springer <felixspringer149@gmail.com>
2020-03-20 00:13:38 +0100

    removed unnessary dependencies from cabal file and include shell.nix

---
+++ shell.nix
@@ -0,0 +1,34 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, containers, hspec, QuickCheck, stdenv
+      , vector
+      }:
+      mkDerivation {
+        pname = "go";
+        version = "0.1.0.0";
+        src = ./.;
+        isLibrary = true;
+        isExecutable = true;
+        libraryHaskellDepends = [ base containers vector ];
+        executableHaskellDepends = [ base ];
+        testHaskellDepends = [ base hspec QuickCheck ];
+        homepage = "https://github.com/jumper149/go";
+        description = "Go, the abstract strategy board game";
+        license = stdenv.lib.licenses.bsd3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
