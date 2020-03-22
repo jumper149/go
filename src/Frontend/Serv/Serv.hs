4081900691d2ab716d1fd260fb60d121b1c80451
Felix Springer <felixspringer149@gmail.com>
2020-03-22 02:46:42 +0100

    minor syntax fixes

--- src/Frontend/Serv/Serv.hs
+++ src/Frontend/Serv/Serv.hs
@@ -3,27 +3,24 @@
 module Frontend.Serv.Serv ( ServGame (runServer)
                           ) where
 
-import           Data.Aeson
-import           Data.Proxy
-import           GHC.Generics
-import           Network.Wai
-import           Network.Wai.Handler.Warp
-import           Servant
+import Data.Aeson
 import qualified Data.ByteString.Lazy as B
+import Data.Proxy
+import GHC.Generics
+import Network.Wai.Handler.Warp
+import Servant
 
 import Data.Either (fromRight)
 
-import Control.Monad.Trans (lift)
 import Control.Monad.IO.Class
 
 import Game
 import Rules (Rules)
 import State
 
-type Api b c p =
-       "create" :> Post '[JSON] FilePath
-  :<|> "render" :> ReqBody '[JSON] FilePath :> Post '[JSON] (GameState b c p)
-  :<|> "play" :> ReqBody '[JSON] (FilePath,Action c) :> Post '[JSON] ()
+type Api b c p = "create"                                        :> Post '[JSON] FilePath
+            :<|> "render" :> ReqBody '[JSON] FilePath            :> Post '[JSON] (GameState b c p)
+            :<|> "play"   :> ReqBody '[JSON] (FilePath,Action c) :> Post '[JSON] ()
 
 
 class (Game b c p, Generic b, Generic c, Generic p, FromJSON b, FromJSON c, FromJSON p, ToJSON b, ToJSON c, ToJSON p) => ServGame b c p where
@@ -36,7 +33,7 @@ class (Game b c p, Generic b, Generic c, Generic p, FromJSON b, FromJSON c, From
                             maybe (throwError undefined) return gs
           playH (path,action) = do gs <- liftIO $ decode <$> B.readFile path
                                    let newGs = doTurn rules action <$> (gs :: Maybe (GameState b c p))
-                                   maybe (throwError undefined) (liftIO . B.writeFile path . encode . fromRight undefined ) newGs
+                                   maybe (throwError undefined) (liftIO . B.writeFile path . encode . fromRight undefined) newGs
 
   runServer :: Rules -> IO (b,p)
   runServer rules = do putStrLn $ "Port is " <> show port
