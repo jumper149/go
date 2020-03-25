afe9ba8a275beebe9967c91d02ba7eca62fcc3e4
Felix Springer <felixspringer149@gmail.com>
2020-03-25 17:20:37 +0100

    use TypeParameters for Bridge

--- backend/bridge/Bridge.hs
+++ backend/bridge/Bridge.hs
@@ -1,5 +1,6 @@
 import Data.Proxy
 import Language.PureScript.Bridge
+import Language.PureScript.Bridge.TypeParameters
 
 import qualified Go.Board.Default as D
 import qualified Go.Board.Loop as L
@@ -7,13 +8,17 @@ import Go.Game.Game
 import Go.Game.State
 
 types :: [SumType 'Haskell]
-types = [ mkSumType (Proxy :: Proxy (GameState D.BoardSquare D.CoordXY D.PlayerBW))
-        , mkSumType (Proxy :: Proxy (GameState L.BoardLoop L.CoordXY L.PlayerBW))
-        , mkSumType (Proxy :: Proxy (Stone D.PlayerBW))
-        , mkSumType (Proxy :: Proxy (Action D.CoordXY))
+types = [ mkSumType (Proxy :: Proxy (GameState B C P))
+        , mkSumType (Proxy :: Proxy (Stone P))
+        , mkSumType (Proxy :: Proxy (Action C))
+
         , mkSumType (Proxy :: Proxy D.BoardSquare)
         , mkSumType (Proxy :: Proxy D.CoordXY)
         , mkSumType (Proxy :: Proxy D.PlayerBW)
+
+        , mkSumType (Proxy :: Proxy L.BoardLoop)
+        , mkSumType (Proxy :: Proxy L.CoordXY)
+        , mkSumType (Proxy :: Proxy L.PlayerBW)
         ]
 
 main :: IO ()
