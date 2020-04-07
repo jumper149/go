6b55b9b4bce0ba60d3a19deca1b8adcdc0d927d3
Felix Springer <felixspringer149@gmail.com>
2020-04-08 01:20:31 +0200

    nicer coordinates with viewbox and some nice visuals

--- client/Main.hs
+++ client/Main.hs
@@ -80,14 +80,29 @@ viewModel x =
          ]
 
 viewBoard :: D.BoardSquare 2 -> View Action
-viewBoard (D.BSquare _ v) = svg_ [ class_ "board" , viewBox_ "0 0 19 19" ] $ (Prelude.concat . V.toList $ V.imap viewStone v)
+viewBoard (D.BSquare _ v) = svg_ [ class_ "board" , viewBox_ "-0.5 -0.5 21 21" ] $ viewGrid <> viewCoords <> (Prelude.concat . V.toList $ V.imap viewStone v)
+
+-- TODO: Add thicc lines.
+viewGrid :: [View Action]
+viewGrid = fmap (\ x -> line_ [ stroke_ "black" , strokeWidth_ "0.1" , x1_ (ms x) , x2_ (ms x) , y1_ "1" , y2_ "19" ] []) dim1Coords<>
+           fmap (\ y -> line_ [ stroke_ "black" , strokeWidth_ "0.1" , x1_ "1" , x2_ "19" , y1_ (ms y) , y2_ (ms y) ] []) dim1Coords
+  where dim1Coords = [ 1 .. (19 :: Int) ]
+
+-- TODO: Make svg chars and import them here.
+viewCoords :: [View Action]
+viewCoords = fmap (\ y -> ellipse_ [  cx_ "0" , cy_ (ms y) , rx_ "0.4" , ry_ "0.3" ] []) dim1Coords
+          <> fmap (\ y -> ellipse_ [  cx_ "20" , cy_ (ms y) , rx_ "0.4" , ry_ "0.3" ] []) dim1Coords
+          <> fmap (\ x -> ellipse_ [  cx_ (ms x) , cy_ "0" , rx_ "0.3" , ry_ "0.4" ] []) dim1Coords
+          <> fmap (\ x -> ellipse_ [  cx_ (ms x) , cy_ "20" , rx_ "0.3" , ry_ "0.4" ] []) dim1Coords
+  where dim1Coords = [ 1 .. (19 :: Int) ]
 
 viewStone :: KnownNat n => Int -> G.Stone (G.PlayerN n) -> [View Action]
 viewStone _ G.Free = []
 viewStone i (G.Stone p) = [ circle_ [ fill_ (ms $ playerColor p) , cx_ $ ms x , cy_ $ ms y , r_ "0.5" ] []]
-  where y = toEnum (i `div` 19) + 0.5 :: Double
-        x = toEnum (i `mod` 19) + 0.5 :: Double
+  where y = (i `div` 19) + 1 :: Int
+        x = (i `mod` 19) + 1 :: Int
 
+-- TODO: Outsource everything regarding player.
 data Color = Black
            | White
            | Red
