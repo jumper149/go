ee2636ff4ddd7aa23cf06d92900e75f9e0312ae5
Felix Springer <felixspringer149@gmail.com>
2020-05-21 04:00:01 +0200

    server now provides sse

--- server/Server.hs
+++ server/Server.hs
@@ -1,15 +1,19 @@
-{-# LANGUAGE FlexibleContexts, TypeOperators #-}
+{-# LANGUAGE FlexibleContexts, OverloadedStrings, TypeOperators #-}
 
 module Server where
 
+import Control.Concurrent.Chan
 import Control.Concurrent.MVar
 import Control.Monad.IO.Class
 import Control.Monad.Reader
-import Data.Aeson
+import Data.Aeson (encode)
+import Data.Binary.Builder (fromLazyByteString)
 import Data.Default.Class
+import Network.Wai.EventSource (ServerEvent (..), eventSourceAppChan)
 import Network.Wai.Handler.Warp (Port, run)
 import Servant
 import Servant.HTML.Lucid
+import Servant.RawM (RawM)
 import System.Directory (listDirectory)
 
 import Html
@@ -25,28 +29,45 @@ import Go.Run.JSON
 type API b c n =                                           Get '[HTML] GameHtml
             :<|> "render"                               :> Get '[JSON] (GameState b c n)
             :<|> "play"   :> ReqBody '[JSON] (Action c) :> Post '[JSON] ()
+            :<|> "sse"                                  :> RawM
             :<|> "public"                               :> Raw
 
 api :: Proxy (API b c n)
 api = Proxy
 
-type AppM b c n = ReaderT (MVar (GameState b c n)) Handler
+data ServerState b c n = ServerState { mVar :: MVar (GameState b c n)
+                                     , chan :: Chan ServerEvent
+                                     }
+
+updateChan :: (JSONGame b c n, MonadIO m, MonadReader (ServerState b c n) m) => m ()
+updateChan = do gs <- liftIO . readMVar =<< asks mVar
+                event <- asks chan
+                let encoding = ServerEvent { eventName = pure "Update GameState"
+                                           , eventId = pure "0"
+                                           , eventData = pure . fromLazyByteString $ encode gs
+                                           }
+                liftIO $ writeChan event encoding
+
+type AppM b c n = ReaderT (ServerState b c n) Handler
 
 handler :: forall b c n. JSONGame b c n => FilePath -> Config -> ServerT (API b c n) (AppM b c n)
-handler path config = gameH :<|> renderH :<|> playH :<|> publicH
+handler path config = gameH :<|> renderH :<|> playH :<|> sseH :<|> publicH
   where gameH :: Monad m => m GameHtml
         gameH = return GameHtml
 
-        renderH :: (MonadIO m, MonadReader (MVar (GameState b c n)) m)
+        renderH :: (MonadIO m, MonadReader (ServerState b c n) m)
                 => m (GameState b c n)
-        renderH = liftIO . readMVar =<< ask
+        renderH = liftIO . readMVar =<< asks mVar
 
-        playH :: (MonadIO m, MonadReader (MVar (GameState b c n)) m)
+        playH :: (MonadIO m, MonadReader (ServerState b c n) m)
               => Action c
               -> m ()
-        playH action = do gs <- ask
+        playH action = do gs <- asks mVar
                           liftIO $ modifyMVar_ gs f
           where f = return . doTurn (rules config) action
+
+        sseH :: MonadReader (ServerState b c n) m => m Application
+        sseH = eventSourceAppChan <$> asks chan
 
         publicH :: ServerT Raw m
         publicH = serveDirectoryWebApp path
@@ -57,8 +78,9 @@ server port path = do putStrLn $ "Port is: " <> show port
 
                       initial <- either (error . show) id <$> runConfiguredT config initState :: IO (GameState b c n) -- TODO: error?
                       gs <- newMVar initial
+                      event <- newChan
 
-                      let app = serve api $ hoistServer api (\ r -> runReaderT r gs) (handler path config)
+                      let app = serve api $ hoistServer api (\ r -> runReaderT r (ServerState gs event)) (handler path config)
                       run port app
                       return undefined -- TODO: undefined behaviour
   where config = def
