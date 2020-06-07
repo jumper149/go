76b9ba0a1a9df1b1aa02a98f92c884c2dae4b8b3
Felix Springer <felixspringer149@gmail.com>
2020-06-07 03:49:04 +0200

    move Message into own module

---
+++ server/Message.hs
@@ -0,0 +1,28 @@
+module Message ( WSServerMessage (..)
+               , WSClientMessage (..)
+               ) where
+
+import Data.Aeson
+import Data.Maybe (fromMaybe)
+import GHC.Generics
+import Network.WebSockets
+
+import Go.Run.JSON
+
+newtype WSServerMessage b c n = WSServerMessage { unwrapWSServerMessage :: ServerMessage b c n }
+  deriving (Eq, Generic, Ord, Read, Show)
+
+-- TODO: instance only covers text, not binary!
+instance JSONGame b c n => WebSocketsData (WSServerMessage b c n) where
+  fromDataMessage (Text bs _) = fromLazyByteString bs
+  fromLazyByteString = WSServerMessage . fromMaybe ServerMessageFail . decode
+  toLazyByteString = encode . unwrapWSServerMessage
+
+newtype WSClientMessage b c n = WSClientMessage { unwrapWSClientMessage :: ClientMessage b c n }
+  deriving (Eq, Generic, Ord, Read, Show)
+
+-- TODO: instance only covers text, not binary!
+instance JSONGame b c n => WebSocketsData (WSClientMessage b c n) where
+  fromDataMessage (Text bs _) = fromLazyByteString bs
+  fromLazyByteString = WSClientMessage . fromMaybe ClientMessageFail . decode
+  toLazyByteString = encode . unwrapWSClientMessage
--- server/Server.hs
+++ server/Server.hs
@@ -9,22 +9,22 @@ import Data.Aeson
 import qualified Data.Binary.Builder as B
 import qualified Data.ByteString.Lazy as BS
 import Data.Default.Class
-import Data.Maybe (fromMaybe)
 import qualified Data.Text as T
 import GHC.Generics
 import GHC.TypeLits hiding (Text)
-import Network.HTTP.Types.Status
+import Network.HTTP.Types.Status (status400)
 import Network.Wai
 import Network.Wai.EventSource (ServerEvent (..), eventSourceAppChan)
 import Network.Wai.Handler.Warp (Port, run)
 import Network.Wai.Handler.WebSockets (websocketsOr)
 import Network.WebSockets
 import Servant
-import Servant.HTML.Lucid
+import Servant.HTML.Lucid (HTML)
 import Servant.RawM (RawM)
 import System.Directory (listDirectory)
 
 import Html
+import Message
 
 import qualified Go.Board.Default as D
 import Go.Game.Config
@@ -33,24 +33,6 @@ import Go.Game.Game
 import Go.Game.Playing
 import Go.Game.State
 import Go.Run.JSON
-
-newtype WSServerMessage b c n = WSServerMessage { unwrapWSServerMessage :: ServerMessage b c n }
-  deriving (Eq, Generic, Ord, Read, Show)
-
--- TODO: instance only covers text, not binary!
-instance JSONGame b c n => WebSocketsData (WSServerMessage b c n) where
-  fromDataMessage (Text bs _) = fromLazyByteString bs
-  fromLazyByteString = WSServerMessage . fromMaybe ServerMessageFail . decode
-  toLazyByteString = encode . unwrapWSServerMessage
-
-newtype WSClientMessage b c n = WSClientMessage { unwrapWSClientMessage :: ClientMessage b c n }
-  deriving (Eq, Generic, Ord, Read, Show)
-
--- TODO: instance only covers text, not binary!
-instance JSONGame b c n => WebSocketsData (WSClientMessage b c n) where
-  fromDataMessage (Text bs _) = fromLazyByteString bs
-  fromLazyByteString = WSClientMessage . fromMaybe ClientMessageFail . decode
-  toLazyByteString = encode . unwrapWSClientMessage
 
 type API =             Get '[HTML] GameHtml
       :<|> "wss"    :> RawM
