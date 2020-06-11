d4f2302a8e3292fadf76daf79c969427e63ccb99
Felix Springer <felixspringer149@gmail.com>
2020-06-11 18:16:40 +0200

    some renaming

--- server/Server.hs
+++ server/Server.hs
@@ -35,29 +35,29 @@ import Go.Run.JSON
 
 newtype ServerState b c n = ServerState { gsMVar :: MVar (GameState b c n) }
 
-newtype AppM b c n a = AppM { unwrapAppM :: ReaderT (ServerState b c n) Handler a }
+newtype HandlerM b c n a = HandlerM { unwrapHandlerM :: ReaderT (ServerState b c n) Handler a }
   deriving (Applicative, Functor, Monad, MonadIO, MonadReader (ServerState b c n))
 
-runAppM :: ServerState b c n -> AppM b c n a -> Handler a
-runAppM ss = flip runReaderT ss . unwrapAppM
+runHandlerM :: ServerState b c n -> HandlerM b c n a -> Handler a
+runHandlerM ss = flip runReaderT ss . unwrapHandlerM
 
-handler :: forall b c n. JSONGame b c n => FilePath -> Config -> ServerT API (AppM b c n)
+handler :: forall b c n. JSONGame b c n => FilePath -> Config -> ServerT API (HandlerM b c n)
 handler path config = gameH :<|> wssH :<|> publicH
   where gameH :: Monad m => m GameHtml
         gameH = return GameHtml
 
         wssH :: (MonadIO m, MonadReader (ServerState b c n) m) => m Application
         wssH = do gs <- asks gsMVar
-                  return $ websocketsOr defaultConnectionOptions (wsApp gs) backupApp
-          where wsApp :: MVar (GameState b c n) -> PendingConnection -> IO ()
-                wsApp gsMVar pendingConn = do conn <- acceptRequest pendingConn
-                                              gs <- readMVar gsMVar
-                                              sendTextData conn . WSServerMessage $ ServerMessageGameState gs
-                                              clientMsg <- unwrapWSClientMessage <$> receiveData conn :: IO (ClientMessage b c n)
-                                              BS.putStrLn $ encode clientMsg
-                                              case clientMsg of
-                                                ClientMessageFail -> putStrLn "failed to do action"
-                                                ClientMessageAction action -> modifyMVar_ gsMVar (return . doTurn (rules config) action) -- TODO: broadcast
+                  return $ websocketsOr defaultConnectionOptions (handler gs) backupApp
+          where handler :: MVar (GameState b c n) -> PendingConnection -> IO ()
+                handler gsMVar pendingConn = do conn <- acceptRequest pendingConn
+                                                gs <- readMVar gsMVar
+                                                sendTextData conn . WSServerMessage $ ServerMessageGameState gs
+                                                clientMsg <- unwrapWSClientMessage <$> receiveData conn :: IO (ClientMessage b c n)
+                                                BS.putStrLn $ encode clientMsg
+                                                case clientMsg of
+                                                  ClientMessageFail -> putStrLn "failed to do action"
+                                                  ClientMessageAction action -> modifyMVar_ gsMVar (return . doTurn (rules config) action) -- TODO: broadcast
 
                 backupApp :: Application
                 backupApp _ respond = respond $ responseLBS status400 [] "Not a WebSocket request"
@@ -72,7 +72,7 @@ server port path = do putStrLn $ "Port is: " <> show port
                       initial <- either (error . show) id <$> runConfiguredT config initState :: IO (GameState b c n) -- TODO: error?
                       gs <- newMVar initial
 
-                      let app = serve api $ hoistServer api (runAppM $ ServerState gs) (handler path config)
+                      let app = serve api $ hoistServer api (runHandlerM $ ServerState gs) (handler path config)
                       run port app
                       return undefined -- TODO: undefined behaviour
   where config = def
