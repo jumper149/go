57ba56af3e7347bffd4a17a00b773ab2e6a3164d
Felix Springer <felixspringer149@gmail.com>
2020-06-21 02:54:16 +0200

    rename rule violations more clearly

--- src/Go/Game/Act.hs
+++ src/Go/Game/Act.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE FlexibleContexts, RecordWildCards #-}
 
-module Go.Game.Act ( Exception (..)
+module Go.Game.Act ( RuleViolation (..)
                    , act
                    , initState
                    ) where
@@ -18,7 +18,7 @@ import Go.Game.Rules
 import Go.Game.State
 
 -- | Apply action to 'GameState' and handle all fields in 'GameState'. Doesn't check rules.
-act :: forall b c n. Game b c n => Rules -> Action c -> GameState b c n -> Either Exception (GameState b c n)
+act :: forall b c n. Game b c n => Rules -> Action c -> GameState b c n -> Either RuleViolation (GameState b c n)
 act rls action gs = checkRules rls $
   GState { currentBoard = case action of
                             Pass -> currentBoard gs
@@ -33,18 +33,18 @@ act rls action gs = checkRules rls $
          }
 
 -- | A data type representing exceptions, that may occur in 'checkRules'.
-data Exception = ExceptEnd
-               | ExceptPassing
-               | ExceptNotFree
-               | ExceptSuicide
-               | ExceptKo
+data RuleViolation = ExceptionEnd
+                   | RuleViolationPassing
+                   | RuleViolationNotFree
+                   | RuleViolationSuicide
+                   | RuleViolationKo
   deriving (Bounded, Enum, Eq, Generic, Ord, Read, Show)
 
-instance FromJSON Exception
-instance ToJSON Exception
+instance FromJSON RuleViolation
+instance ToJSON RuleViolation
 
 -- | Check rules. This should be done after applying an 'Action' to the 'GameState' with 'act'.
-checkRules :: Game b c n => Rules -> GameState b c n -> Either Exception (GameState b c n)
+checkRules :: Game b c n => Rules -> GameState b c n -> Either RuleViolation (GameState b c n)
 checkRules rls gs = flip runReaderT gs . runRulesetEnvT rls $ do
   checkFree
   checkPassing
@@ -53,27 +53,27 @@ checkRules rls gs = flip runReaderT gs . runRulesetEnvT rls $ do
   return gs
 
 -- | Check that the last placed stone was placed on a free coordinate.
-checkFree :: (Game b c n, MonadError Exception m, MonadReader (GameState b c n) m) => m ()
+checkFree :: (Game b c n, MonadError RuleViolation m, MonadReader (GameState b c n) m) => m ()
 checkFree = do GState {..} <- ask
                case lastAction of
                  Pass -> return ()
                  Place c -> case previousBoards of
                               previousBoard : _ -> when (getStone previousBoard c /= Free) $
-                                                     throwError ExceptNotFree
+                                                     throwError RuleViolationNotFree
                               _ -> return ()
 
 -- | Check if the number of consecutive passes is below the number of players.
-checkPassing :: forall b c m n. (Game b c n, MonadError Exception m, MonadReader (GameState b c n) m, MonadRules m) => m ()
+checkPassing :: forall b c m n. (Game b c n, MonadError RuleViolation m, MonadReader (GameState b c n) m, MonadRules m) => m ()
 checkPassing = do GState {..} <- ask
                   rPassing <- passing <$> rules
                   case rPassing of
                     Allowed -> when (consecutivePasses >= natVal (Proxy :: Proxy n)) $
-                                 throwError ExceptEnd
+                                 throwError ExceptionEnd
                     Forbidden -> when (lastAction == Pass) $
-                                   throwError ExceptPassing
+                                   throwError RuleViolationPassing
 
 -- | Check that the last placed stone wasn't removed right after, because it didn't have any liberties.
-checkSuicide :: (Game b c n, MonadError Exception m, MonadReader (GameState b c n) m, MonadRules m) => m ()
+checkSuicide :: (Game b c n, MonadError RuleViolation m, MonadReader (GameState b c n) m, MonadRules m) => m ()
 checkSuicide = do GState {..} <- ask
                   rSuicide <- suicide <$> rules
                   case rSuicide of
@@ -81,16 +81,16 @@ checkSuicide = do GState {..} <- ask
                     Forbidden -> case lastAction of
                                    Pass -> return ()
                                    Place c -> when (getStone currentBoard c == Free) $
-                                                throwError ExceptSuicide
+                                                throwError RuleViolationSuicide
 
 -- | Check if the last applied action is correct regarding to the ko-rule 'ko'.
-checkKo :: (Game b c n, MonadError Exception m, MonadReader (GameState b c n) m, MonadRules m) => m ()
+checkKo :: (Game b c n, MonadError RuleViolation m, MonadReader (GameState b c n) m, MonadRules m) => m ()
 checkKo = do GState {..} <- ask
              rKo <- ko <$> rules
              case rKo of
                Ko Allowed -> return ()
                Ko Forbidden -> case previousBoards of
                                  _ : compareBoard : _ -> when (currentBoard == compareBoard) $
-                                                           throwError ExceptKo
+                                                           throwError RuleViolationKo
                                  _ -> return ()
                SuperKo -> undefined -- TODO implement carefully
