41ed50e9cf1566fc25e779f08281ded336473bc5
Felix Springer <felixspringer149@gmail.com>
2020-07-03 17:49:58 +0200

    add explaining comment

--- server/GameSet/Internal/Identification.hs
+++ server/GameSet/Internal/Identification.hs
@@ -3,5 +3,7 @@ module GameSet.Internal.Identification ( GameId
 
 import GHC.Generics
 
+-- extra module to fix cyclic dependency is Client.Internal and GameSet.Internal
+
 newtype GameId = GameId { unwrapGameId :: Integer }
   deriving (Enum, Eq, Generic, Ord, Read, Show)
