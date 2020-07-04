7b6bb5c1c8bb80596c84d1c6ddcd2d7d14421504
Felix Springer <felixspringer149@gmail.com>
2020-07-04 22:06:39 +0200

    remove unused Message types

--- src/Go/Message.hs
+++ src/Go/Message.hs
@@ -1,47 +1,25 @@
-module Go.Message ( ServerMessageRep (..) -- TODO: remove
-                  , ClientMessageRep (..) -- TODO: remove
-                  , ServerMessage (..)
-                  , ClientMessage (..)
+module Go.Message ( ServerMessageRep (..)
+                  , ClientMessageRep (..)
                   ) where
 
 import Data.Aeson
 import GHC.Generics
 
-import qualified Go.Board.Default as D
 import Go.Game
 import Go.Player
-import Go.Run.JSON
 
-data ServerMessageRep = ServerMessageD_9_2 (AssociatedServerMessage (D.Board 9 2))
-                      | ServerMessageD_13_2 (AssociatedServerMessage (D.Board 13 2))
-                      | ServerMessageRepFail String
+data ServerMessageRep = ServerMessageGameStateRep GameStateRep
+                      | ServerMessagePlayerRep (Maybe PlayerRep)
+                      | ServerMessageFail String
   deriving (Eq, Generic, Ord, Read, Show)
 
 instance FromJSON ServerMessageRep
 instance ToJSON ServerMessageRep
 
-data ServerMessage = ServerMessageGameStateRep GameStateRep
-                   | ServerMessagePlayerRep (Maybe PlayerRep)
-                   | ServerMessageFail String
-  deriving (Eq, Generic, Ord, Read, Show)
-
-instance FromJSON ServerMessage
-instance ToJSON ServerMessage
-
---gs :: ServerMessageRep -> GameStateRep
-
-data ClientMessageRep = ClientMessageD_9_2 (AssociatedClientMessage (D.Board 9 2))
-                      | ClientMessageD_13_2 (AssociatedClientMessage (D.Board 13 2))
-                      | ClientMessageRepFail String
+data ClientMessageRep = ClientMessageActionRep ActionRep
+                      | ClientMessagePlayerRep (Maybe PlayerRep)
+                      | ClientMessageFail String
   deriving (Eq, Generic, Ord, Read, Show)
 
 instance FromJSON ClientMessageRep
 instance ToJSON ClientMessageRep
-
-data ClientMessage = ClientMessageActionRep GameStateRep
-                   | ClientMessagePlayerRep (Maybe PlayerRep)
-                   | ClientMessageFail String
-  deriving (Eq, Generic, Ord, Read, Show)
-
-instance FromJSON ClientMessage
-instance ToJSON ClientMessage
--- src/Go/Run/JSON.hs
+++ src/Go/Run/JSON.hs
@@ -1,8 +1,6 @@
 {-# LANGUAGE FlexibleContexts, KindSignatures, StandaloneDeriving, UndecidableInstances #-}
 
 module Go.Run.JSON ( JSONGame
-                   , AssociatedServerMessage (..)
-                   , AssociatedClientMessage (..)
                    ) where
 
 import Data.Aeson
@@ -13,26 +11,3 @@ import Go.Game.Game
 import Go.Game.State
 
 class (Game b, Generic b, Generic (AssociatedCoord b), FromJSON b, FromJSON (AssociatedCoord b), ToJSON b, ToJSON (AssociatedCoord b)) => JSONGame b
-
--- TODO: remove all messages from this module?
-data AssociatedServerMessage b = ServerMessageGameState (AssociatedGameState b)
-                               | ServerMessagePlayer (Maybe (AssociatedPlayer b))
-  deriving Generic
-deriving instance (Eq b, Eq (AssociatedCoord b)) => Eq (AssociatedServerMessage b)
-deriving instance (Ord b, Ord (AssociatedCoord b)) => Ord (AssociatedServerMessage b)
-deriving instance (KnownNat (AssociatedPlayerCount b), Read b, Read (AssociatedCoord b)) => Read (AssociatedServerMessage b)
-deriving instance (Show b, Show (AssociatedCoord b)) => Show (AssociatedServerMessage b)
-
-instance JSONGame b => FromJSON (AssociatedServerMessage b) where
-instance JSONGame b => ToJSON (AssociatedServerMessage b) where
-
-data AssociatedClientMessage b = ClientMessageAction (AssociatedAction b)
-                               | ClientMessagePlayer (Maybe (AssociatedPlayer b))
-  deriving Generic
-deriving instance (Eq b, Eq (AssociatedCoord b)) => Eq (AssociatedClientMessage b)
-deriving instance (Ord b, Ord (AssociatedCoord b)) => Ord (AssociatedClientMessage b)
-deriving instance (KnownNat (AssociatedPlayerCount b), Read b, Read (AssociatedCoord b)) => Read (AssociatedClientMessage b)
-deriving instance (Show b, Show (AssociatedCoord b)) => Show (AssociatedClientMessage b)
-
-instance JSONGame b => FromJSON (AssociatedClientMessage b) where
-instance JSONGame b => ToJSON (AssociatedClientMessage b) where
